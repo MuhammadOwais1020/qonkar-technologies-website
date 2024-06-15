2f3dd9a26c37621a6bd224bef9ea6a871dccdd6c
Muhammad Owais <owais.quest@gmail.com>
2024-06-15 20:47:50 +0500

    updated file structure

---
+++ header.php
@@ -0,0 +1,55 @@
+<head>
+  <title>Qonkar Technologies Private Limited | IT Services Company</title>
+  <!-- description for SEO -->
+  <meta name="description" content="Welcome to Qonkar Technologies!
+Qonkar Technologies Private Limited is a premium full-service agency offering Web Development, Design, Digital Marketing, and Production services. Our mission is to  help businesses enhance their online presence through expert IT solution services. 
+
+Our team of domain experts is dedicated to delivering outstanding results. By leveraging innovative strategies and a collaborative approach, we ensure our clients achieve success in today’s competitive market.
+
+Trusted by businesses worldwide, we build strong partnerships by providing custom solutions that drive growth and create real impact.
+
+OUR SERVICES:
+
+• Website Design & Development
+• Custom Software Applications
+• Web Applications
+• Business Websites
+• WordPress Development
+• Shopify ECommerce Store Setup/Development
+• Shopify Theme Customizations
+• Landing Page Designs
+• Design & Branding
+• User Interface
+• Digital Marketing
+• Social Media Management 
+• Social Media Content 
+• Product Photography & Videography
+" />
+
+  <meta name="robots" content="index, follow" />
+
+  <link rel="canonical" href="" />
+  <meta name="author" content="Qonkar Technologies Private Limited" />
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <link rel="icon" href="assets/images/icons/qonkar-technologies-private-limited.jpg" type="image/x-icon" />
+
+  <meta property="og:title" content="Qonkar Technologies Private Limited | IT services company" />
+  <!-- description for social media sharing -->
+  <meta property="og:description" content="Qonkar Technologies Private Limited!" />
+  <meta property="og:url" content="https://www.qonkar.com/" />
+  <meta property="og:image"
+    content="https://www.qonkar.com/assets/images/social-media-posts/qonkar-technologies-private-limited-services.jpg" />
+
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:site" content="@yourcompanytwitter" />
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+  <!-- css -->
+  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="css/responsive.css" />
+  <link rel="stylesheet" href="css/swiper.css" />
+</head>
---
+++ index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include('header.php');
+?>
+<body>
+ <?php
+ include('nav-bar.php')
+ ?>
+  <section class="hero-section">
+    <div class="container">
+      <h1 class="tagline-heading">An <span class="text-secondary">IT</span> Services Copmany</h1>
+      <h2 class="slogan">Where Dreams <span class="text-secondary">Go</span> Digital!</h2>
+      <h2 class="services-heading text-secondary">Website Design & Development</h2>
+    </div>
+  </section>
+
+  <section class="trusted-companies">
+    <h1 class="section-heading">Trusted by</h1>
+    <div class="outer-container">
+      <div class="container">
+        <div class="logos">
+          <img src="assets/images/logo/trusted-by/carsound.png"
+            alt="qonkar technologies pvt ltd trusted by carsound denmark" />
+          <img src="assets/images/logo/trusted-by/fuel.png" alt="qonkar technologies pvt ltd trusted by fuel" />
+          <img src="assets/images/logo/trusted-by/microsoft.png"
+            alt="qonkar technologies pvt ltd trusted by microsoft" />
+          <img src="assets/images/logo/trusted-by/saala.png" alt="qonkar technologies pvt ltd trusted by saala" />
+          <img src="assets/images/logo/trusted-by/shopify.png" alt="qonkar technologies pvt ltd trusted by shopify" />
+          <img src="assets/images/logo/trusted-by/upwork.png" alt="qonkar technologies pvt ltd trusted by upwork" />
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="services-section" id="services-section">
+    <div class="container">
+      <h1 class="section-heading">Our Services</h1>
+      <div class="services">
+        <div class="service">
+          <img src="assets/images/logo/services/website-design-development.png"
+            alt="website design & development service by qonkar technologies pvt ltd" />
+          <h2 class="sub-heading">Website Design & Development</h2>
+          <p class="service-description">
+            Custom Software Applications <br />
+            Web Applications <br />
+            Business Websites Development <br />
+            WordPress Development <br />
+            Shopify Store Development <br />
+            Landing Page Designs
+          </p>
+        </div>
+        <div class="service">
+          <img src="assets/images/logo/services/digital-marketing.png"
+            alt="Digtial Marketing service by qonkar technologies pvt ltd" />
+          <h2 class="sub-heading">Digtial Marketing</h2>
+          <p class="service-description">
+            Custom Software Applications <br />
+            Web Applications <br />
+            Business Websites Development <br />
+            WordPress Development <br />
+            Shopify Store Development <br />
+            Landing Page Designs
+          </p>
+        </div>
+        <div class="service">
+          <img src="assets/images/logo/services/user-interface.png"
+            alt="User Interface service by qonkar technologies pvt ltd" />
+          <h2 class="sub-heading">User Interface</h2>
+          <p class="service-description">
+            Custom Software Applications <br />
+            Web Applications <br />
+            Business Websites Development <br />
+            WordPress Development <br />
+            Shopify Store Development <br />
+            Landing Page Designs
+          </p>
+        </div>
+        <div class="service">
+          <img src="assets/images/logo/services/design-branding.png"
+            alt="design & branding service by qonkar technologies pvt ltd" />
+          <h2 class="sub-heading">Design & Branding</h2>
+          <p class="service-description">
+            Custom Software Applications <br />
+            Web Applications <br />
+            Business Websites Development <br />
+            WordPress Development <br />
+            Shopify Store Development <br />
+            Landing Page Designs
+          </p>
+        </div>
+        <div class="service">
+          <img src="assets/images/logo/services/social-media-management.png"
+            alt="Social Media Management service by qonkar technologies pvt ltd" />
+          <h2 class="sub-heading">Social Media Management</h2>
+          <p class="service-description">
+            Custom Software Applications <br />
+            Web Applications <br />
+            Business Websites Development <br />
+            WordPress Development <br />
+            Shopify Store Development <br />
+            Landing Page Designs
+          </p>
+        </div>
+        <div class="service">
+          <img src="assets/images/logo/services/photography-videography.png"
+            alt="Photography & Videography service by qonkar technologies pvt ltd" />
+          <h2 class="sub-heading">Photography & Videography</h2>
+          <p class="service-description">
+            Custom Software Applications <br />
+            Web Applications <br />
+            Business Websites Development <br />
+            WordPress Development <br />
+            Shopify Store Development <br />
+            Landing Page Designs
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="our-work-section display-none">
+    <h1 class="section-heading">Our Work</h1>
+    <!-- Swiper -->
+    <div class="our-work-wrapper">
+      <div class="swiper-wrapper">
+        <img src="assets/images/services/Services-Qonkar-Technologies-Pvt-Ltd.png" alt="" />
+        <img src="assets/images/services/Services-Qonkar-Technologies-Pvt-Ltd.png" alt="" />
+        <img src="assets/images/services/Services-Qonkar-Technologies-Pvt-Ltd.png" alt="" />
+        <img src="assets/images/services/Services-Qonkar-Technologies-Pvt-Ltd.png" alt="" />
+        <img src="assets/images/services/Services-Qonkar-Technologies-Pvt-Ltd.png" alt="" />
+        <img src="assets/images/services/Services-Qonkar-Technologies-Pvt-Ltd.png" alt="" />
+        <img src="assets/images/services/Services-Qonkar-Technologies-Pvt-Ltd.png" alt="" />
+        <img src="assets/images/services/Services-Qonkar-Technologies-Pvt-Ltd.png" alt="" />
+      </div>
+    </div>
+  </section>
+
+  <!-- Swiper JS -->
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+  <section class="trusted-companies">
+    <h1 class="section-heading">Client We Serve</h1>
+    <div class="outer-container">
+      <div class="container">
+        <div class="logos">
+          <img src="assets/images/logo/trusted-by/carsound.png"
+            alt="qonkar technologies pvt ltd trusted by carsound denmark" />
+          <img src="assets/images/logo/trusted-by/fuel.png" alt="qonkar technologies pvt ltd trusted by fuel" />
+          <img src="assets/images/logo/trusted-by/microsoft.png"
+            alt="qonkar technologies pvt ltd trusted by microsoft" />
+          <img src="assets/images/logo/trusted-by/saala.png" alt="qonkar technologies pvt ltd trusted by saala" />
+          <img src="assets/images/logo/trusted-by/shopify.png" alt="qonkar technologies pvt ltd trusted by shopify" />
+          <img src="assets/images/logo/trusted-by/upwork.png" alt="qonkar technologies pvt ltd trusted by upwork" />
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="call-to-action-section">
+    <div class="container">
+      <div class="details">
+        <h2>Where Dreams <span class="text-secondary">Go</span> Digital!</h2>
+        <p>
+          Experience the power of turning your digital <span class="text-secondary">dreams</span> into tangible results.
+          <br />
+          We offer comprehensive <span class="text-secondary">IT solutions</span> to empower your <span
+            class="text-secondary">vision</span>.
+        </p>
+      </div>
+      <div class="button">
+        <a href="contact-us.html" class="btn">Lets Get Started!</a>
+      </div>
+    </div>
+  </section>
+
+  <section class="testimonial-section">
+    <div class="container">
+      <div class="details">
+        <h2>What our <span class="text-secondary">client says</span></h2>
+        <p>Don`t just take our words for it - <span class="text-secondary">Take theirs!</span></p>
+      </div>
+
+      <div class="review-box">
+        <div class="quote">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path
+              d="M0 216C0 149.7 53.7 96 120 96h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V320 288 216zm256 0c0-66.3 53.7-120 120-120h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H320c-35.3 0-64-28.7-64-64V320 288 216z" />
+          </svg>
+        </div>
+        <div class="stars">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+            <path
+              d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+            <path
+              d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+            <path
+              d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+            <path
+              d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+            <path
+              d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+          </svg>
+        </div>
+        <div class="review">
+          <p>We partnered with Qonkar Technologies to develop a custom e-commerce platform for our business. Their team
+            was incredibly professional and responsive throughout the entire process. They listened to our needs and
+            delivered a solution that has significantly increased our online sales and streamlined our order fulfilment
+            process. We highly recommend Qonkar Technologies for any business looking for a reliable and
+            results-oriented IT partner.</p>
+        </div>
+        <div class="author">
+          <p class="text-secondary">John Smith, CEO of Acme Inc.</p>
+        </div>
+        <div class="navigation-button">
+          <div class="previous">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path
+                d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+            </svg>
+          </div>
+          <div class="next">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path
+                d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <?php
+  include('footer.php')
+  ?>
+  <script src="assets/script.js"></script>
+</body>
+
+</html>
