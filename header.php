dbf1025108150fdb60b74027b1a48ee325249a50
Muhammad Owais <owais.quest@gmail.com>
2024-06-22 22:21:31 +0500

    Technical SEO

--- header.php
+++ header.php
@@ -1,8 +1,23 @@
 <head>
   <title>Qonkar Technologies Private Limited | IT Services Company</title>
   <!-- description for SEO -->
-  <meta name="description" content="Grow Your Business Online with Qonkar Technologies - Full-Service Digital Agency
-  Grow your business online with Qonkar Technologies! We're a results-oriented digital agency specializing in web design, SEO, social media marketing, and branding. Get a user-friendly website, high-performing content, and dominate search engine results. We help you attract more leads, convert visitors into customers, and achieve your online marketing goals. 
+  <meta name="description" content="Qonkar Technologies (Pvt) Limited is a premium web design agency and digital agency based in Pakistan. We specialize in transforming your digital presence with cutting-edge technology and innovative strategies. Our team of dedicated professionals is passionate about delivering top-notch website design & development services to help your business thrive in the digital age.
+At Qonkar Technologies, our mission is to empower businesses with robust and effective digital solutions. We understand that in today's competitive market, having a strong online presence is crucial. That's why we offer a comprehensive range of services designed to enhance your brand, engage your audience, and drive growth. Our goal is to provide our clients with the tools and support they need to succeed online.
+Our Services:
+Website Design & Development: Creating responsive, user-friendly websites tailored to your business needs.
+Ecommerce Solutions: Building secure and scalable online stores, including specialized services to build Shopify websites.
+Design & Branding: Crafting unique and memorable brand identities that stand out.
+UI/UX Design: Enhancing user experience with intuitive and attractive interfaces.
+Digital Marketing: Implementing effective strategies to boost your online presence and reach.
+Social Media Management (SMM): Managing and growing your social media profiles with targeted strategies.
+Social Media Content Creation: Producing engaging and high-quality content for your social media platforms.
+SEO Services: Optimizing your website to rank higher on search engines and attract more organic traffic.
+Photography & Videography: Capturing stunning visuals to elevate your brand’s image.
+
+We serve a variety of industries with customized digital solutions. For real estate owners, restaurant owners, healthcare providers, educational institutions, travel agencies, event management companies, gym owners, retail businesses, and e-commerce businesses, we enhance visibility, attract customers, and ensure online success through tailored strategies and professional branding.
+As a leading web design firm and website development services provider in Pakistan, Qonkar Technologies is dedicated to offering innovative solutions tailored to your business needs. We are experts in crafting effective social media strategies, creating high-converting Shopify websites, and delivering comprehensive digital marketing services to help you achieve your business goals.
+Partner with Qonkar Technologies (Pvt) Limited and take your business to the next level. Let’s build your digital future together!
+
 " />
 
   <meta name="robots" content="index, follow" />
@@ -16,7 +31,7 @@
 
   <meta property="og:title" content="Qonkar Technologies Private Limited | IT services company" />
   <!-- description for social media sharing -->
-  <meta property="og:description" content="Qonkar Technologies Private Limited!" />
+  <meta property="og:description" content="Qonkar Technologies (Pvt) Limited is a premium web design agency offering top-tier website development services. As a leading digital agency, we excel in design & branding, digital marketing, and SEO, ensuring your brand stands out. Our expertise in social media strategy and social media management guarantees engaging online presence. Qonkar also specializes in user-friendly interface (UI) design, and high-quality photography & videography, providing a comprehensive digital solution." />
   <meta property="og:url" content="https://www.qonkar.com/" />
   <meta property="og:image"
     content="https://www.qonkar.com/assets/images/social-media-posts/qonkar-technologies-private-limited-services.jpg" />
