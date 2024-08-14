ddcb60679ad0cf90c2d92fbc3a8478bfc317db8b
Afra-Parveen-Suleman <155308926+Afra-Parveen-Suleman@users.noreply.github.com>
2024-08-14 13:19:45 +0500

    career-apply-now-form-added

--- career-apply-fetch-data.php
+++ career-apply-fetch-data.php
@@ -1,18 +1,22 @@
 <?php
 
-//DB-connection-file
+// DB connection file
 include('connection.php');
 
-
-
-// Check if form data is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $fullName = mysqli_real_escape_string($conn, $_POST['first-name']);
     $whatsappNumber = mysqli_real_escape_string($conn, $_POST['number']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $country = mysqli_real_escape_string($conn, $_POST['country']);
     $qualification = mysqli_real_escape_string($conn, $_POST['qualification']);
-    $skills = isset($_POST['skills']) ? mysqli_real_escape_string($conn, $_POST['skills']) : '';
+    
+    if (isset($_POST['skills']) && is_array($_POST['skills'])) {
+        $skills = array_map([$conn, 'real_escape_string'], $_POST['skills']);
+        $skillsString = implode(',', $skills);
+    } else {
+        $skillsString = '';
+    }
+
     $applyingFor = mysqli_real_escape_string($conn, $_POST['applying-for']);
     $description = mysqli_real_escape_string($conn, $_POST['description']);
     $outlineSkills = mysqli_real_escape_string($conn, $_POST['outline-skills']);
@@ -44,9 +48,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             exit;
         }
     }
+
     $query = "
         INSERT INTO Career_Opportunity_applyNow (full_name, whatsapp_number, email, country, qualification, skills, applying_for, description, outline_skills, experties, time_period, future_goal, resume, portfolio)
-        VALUES ('$fullName', '$whatsappNumber', '$email', '$country', '$qualification', '$skills', '$applyingFor', '$description', '$outlineSkills', '$experties', '$timePeriod', '$futureGoal', '$resumePath', '$portfolioPath')
+        VALUES ('$fullName', '$whatsappNumber', '$email', '$country', '$qualification', '$skillsString', '$applyingFor', '$description', '$outlineSkills', '$experties', '$timePeriod', '$futureGoal', '$resumePath', '$portfolioPath')
     ";
 
     if (mysqli_query($conn, $query)) {
@@ -57,8 +62,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 } else {
     echo 'Invalid request method.';
 }
-
-
 
 mysqli_close($conn);
 ?>
