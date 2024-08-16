666360098dd883159b4c3786b6fe5b7033806370
Afra-Parveen-Suleman <155308926+Afra-Parveen-Suleman@users.noreply.github.com>
2024-08-16 18:16:10 +0500

    pages-added

---
+++ career-opportunity-details.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php
+include('header.php');
+?>
+  <title>Careers | IT Services Company</title>
+</head>
+<body>
+      <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M436PKCG"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+    
+     <?php
+     include('nav-bar.php')
+     ?>
+ <section class="container">       
+    <div class="career-details">
+          <h1>Graphic Designer</h1>
+          <div class="career-detail-block">
+              <div class="detail-blocks">
+                <div class="detail-block">
+                  <h4>Experience</h4>
+                  <p>2 Year</p>
+                </div>
+                <div class="detail-block">
+                  <h4>Salary</h4>
+                  <p>100K PKR</p>
+                </div>
+                <div class="detail-block">
+                  <h4>Type</h4>
+                  <p>Full Time</p>
+                </div>
+              </div>
+              <div class="career-details-button">
+                <a href="career-opportunity-applynow.php"><button class="btn full-width">Apply Now</button></a>
+              </div>
+          </div>
+    </div>
+</section>
+  <?php
+  include('footer.php')
+  ?>
+</body>
+</html>
---
+++ career-opportunity.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php
+include('header.php');
+?>
+  <title>Careers | IT Services Company</title>
+</head>
+<body>
+      <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M436PKCG"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+    
+     <?php
+     include('nav-bar.php')
+     ?>
+    <!-- kjhkjkj -->
+  <section class="career-section">
+    <div class="container">
+      <h1>Open Positions</h1>
+      <div class="career-blocks">
+          <div class="career-head">
+            <h4>Name: Graphic Designers</h4>
+            <p>Experience: 0 Years<br />
+            Type: Internship<br /></p>
+          </div>
+          <div class="career-button">
+            <a href="career-opportunity-details.php"><button class="btn">Details</button></a>
+            <a href="career-opportunity-applynow.php"><button class="btn">Apply Now</button></a>
+          </div>
+      </div>
+      <div class="career-blocks">
+          <div class="career-head">
+            <h4>Name: Video Editors</h4>
+            <p>Experience: 0 Years<br />
+            Type: Internship<br /></p>
+          </div>
+          <div class="career-button">
+            <a href="career-opportunity-details.php"><button class="btn">Details</button></a>
+            <a href="career-opportunity-applynow.php"><button class="btn">Apply Now</button></a>
+          </div>
+      </div>
+      <div class="career-blocks">
+          <div class="career-head">
+            <h4>Name: SEO Specialists</h4>
+            <p>Experience: 3-6 Months<br />
+            Type: Full Time<br /></p>
+          </div>
+          <div class="career-button">
+            <a href="career-opportunity-details.php"><button class="btn">Details</button></a>
+            <a href="career-opportunity-applynow.php"><button class="btn">Apply Now</button></a>
+          </div>
+      </div>
+      <div class="career-blocks">
+          <div class="career-head">
+            <h4>Name: Social Media Managers</h4>
+            <p>Experience: 0 Years<br />
+            Type: Internship<br /></p>
+          </div>
+          <div class="career-button">
+            <a href="career-opportunity-details.php"><button class="btn">Details</button></a>
+            <a href="career-opportunity-applynow.php"><button class="btn">Apply Now</button></a>
+          </div>
+      </div>
+      <div class="career-blocks">
+          <div class="career-head">
+            <h4>Name: Shopify Developer</h4>
+            <p>Experience: 2 Years<br />
+            Type: Full Time<br /></p>
+          </div>
+          <div class="career-button">
+            <a href="career-opportunity-details.php"><button class="btn">Details</button></a>
+            <a href="career-opportunity-applynow.php"><button class="btn">Apply Now</button></a>
+          </div>
+      </div>
+      <div class="career-blocks">
+          <div class="career-head">
+            <h4>Name: Content Writers</h4>
+            <p>Experience: 3-6 Years<br />
+            Type: Internship<br /></p>
+          </div>
+          <div class="career-button">
+            <a href="career-opportunity-details.php"><button class="btn">Details</button></a>
+            <a href="career-opportunity-applynow.php"><button class="btn">Apply Now</button></a>
+          </div>
+      </div>
+      <div class="career-blocks">
+          <div class="career-head">
+            <h4>Name: Email Marketers</h4>
+            <p>Experience: 1-2 Years<br />
+            Type: Project Based<br /></p>
+          </div>
+          <div class="career-button">
+            <a href="career-opportunity-details.php"><button class="btn">Details</button></a>
+            <a href="career-opportunity-applynow.php"><button class="btn">Apply Now</button></a>
+          </div>
+      </div>
+      <div class="career-blocks">
+          <div class="career-head">
+            <h4>Name: Lead Generation</h4>
+            <p>Experience: 0 Years<br />
+            Type: Internship<br /></p>
+          </div>
+          <div class="career-button">
+            <a href="career-opportunity-details.php"><button class="btn">Details</button></a>
+            <a href="career-opportunity-applynow.php"><button class="btn">Apply Now</button></a>
+          </div>
+      </div>
+      <div class="career-blocks">
+          <div class="career-head">
+            <h4>Name: Business Development Representative</h4>
+            <p>Experience: 1-2 Years<br />
+            Type: Project Based<br /></p>
+          </div>
+          <div class="career-button">
+            <a href="career-opportunity-details.php"><button class="btn">Details</button></a>
+            <a href="career-opportunity-applynow.php"><button class="btn">Apply Now</button></a>
+          </div>
+      </div>
+      <div class="career-blocks">
+          <div class="career-head">
+            <h4>Name: MERN Stack Developer</h4>
+            <p>Experience: 2 Years<br />
+            Type: Full Time<br /></p>
+          </div>
+          <div class="career-button">
+            <a href="career-opportunity-details.php"><button class="btn">Details</button></a>
+            <a href="career-opportunity-applynow.php"><button class="btn">Apply Now</button></a>
+          </div>
+      </div>
+      <div class="career-blocks">
+          <div class="career-head">
+            <h4>Name: Front-end Developer</h4>
+            <p>Experience: 2 Years<br />
+            Type: Internship<br /></p>
+          </div>
+          <div class="career-button">
+            <a href="career-opportunity-details.php"><button class="btn">Details</button></a>
+            <a href="career-opportunity-applynow.php"><button class="btn">Apply Now</button></a>
+          </div>
+      </div>
+      <div class="career-blocks">
+          <div class="career-head">
+            <h4>Name: Web Developer</h4>
+            <p>Experience: 0 Years<br />
+            Type: Internship<br /></p>
+          </div>
+          <div class="career-button">
+            <a href="career-opportunity-details.php"><button class="btn">Details</button></a>
+            <a href="career-opportunity-applynow.php"><button class="btn">Apply Now</button></a>
+          </div>
+      </div>
+    </div>
+
+  </section>
+  <?php
+  include('footer.php')
+  ?>
+</body>
+</html>
