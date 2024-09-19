82a3188135a011a9106954658f1f895b676c0d6f
Muhammad Owais <owais.quest@gmail.com>
2024-09-19 21:00:25 +0500

    converted to php

---
+++ v2/index.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Qonkar Technologies | IT Services Company</title>
+
+  <?php
+  include('./sections/head.php');
+  ?>
+</head>
+
+<body>
+<?php
+include('./sections/progress-bar.php')
+?>
+  
+  <div class="main container">
+  
+  <?php
+  include('./sections/nav-bar.php');
+  ?>
+  
+
+    <section class="hero-section">
+      <div class="hero-section-title">
+        <div class="hero-head">
+          <h1 class="heading hero-heading">
+            An <strong>IT Services</strong>  Company
+          </h1>
+          <div class="hero-svg">
+            <svg width="297" height="35" class="svg-path" viewBox="0 0 297 35" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M1.22961 19.3056C3.69595 15.4548 12.9448 12.4865 17.2609 11.4837C36.9917 6.67039 68.7458 4.26347 82.0024 5.46693C95.2591 6.67039 80.7693 9.07692 68.1292 11.4836C55.4892 13.8904 64.738 15.0937 76.1449 13.8904C87.5517 12.687 111.29 11.4836 131.638 10.882C151.985 10.2803 131.329 9.07692 111.29 9.07692C91.2513 9.07692 91.5595 6.67029 98.6503 5.46693C105.741 4.26357 121.772 -0.549874 140.578 0.0518062C159.384 0.653487 203.778 1.85685 222.893 6.06861C242.007 10.2804 281.468 21.7123 291.025 25.3224C300.583 28.9325 299.349 39.161 282.393 33.1442C265.437 27.1274 230.292 16.2972 153.218 14.4921C76.1449 12.6871 17.5691 22.9157 11.4032 27.1274C5.23738 31.3392 0.921268 31.3392 3.38762 27.7291C5.85396 24.119 6.47059 23.5174 4.00425 24.7207C1.5379 25.9241 -1.85333 24.119 1.22961 19.3056Z"
+                fill="#8AD2DC" />
+            </svg>
+          </div>
+
+          <h1 class="heading subheading">Where Dreams Go  Digital!</h1>
+        </div>
+        <p class="paragraph hero-paragraph">
+          Leverage our unmatched fractional demand and creative team to
+          optimize advertisements, launch creative campaigns, and transform
+          reporting.
+        </p>
+        <div class="button hero-button">Book A Strategy Call</div>
+      </div>
+      <div class="hero-small-circle">
+        <img src="./assets/image/small-circle.png" alt="" />
+      </div>
+      <div class="hero-section-image">
+        <div class="wrapper-image">
+          <img src="./assets/image/circle.png" alt="" />
+        </div>
+        <div class="wrapper-inner-image">
+          <img src="./assets/image/prototype.png" alt="" />
+        </div>
+      </div>
+    </section>
+
+    <section class="section-logos">
+      <div class="logos-container margin-top">
+        <div class="home-logo-wrapper bottom">
+          <div class="clients-grid logo-animate-alt mobilegridlogo">
+            <img alt=""
+              src="https://sachellecollective.myshopify.com/cdn/shop/files/SachelleCollective_Logo_Wordmark-Black_54db1d61-f86f-4683-bc7b-ea83a6df4db4.jpg?v=1725828034&width=140"
+              loading="eager" class="client-logo-2" /><img alt=""
+              src="https://sachellecollective.myshopify.com/cdn/shop/files/SachelleCollective_Logo_Wordmark-Black_54db1d61-f86f-4683-bc7b-ea83a6df4db4.jpg?v=1725828034&width=140"
+              loading="eager" class="client-logo-2" /><img alt=""
+              src="https://sachellecollective.myshopify.com/cdn/shop/files/SachelleCollective_Logo_Wordmark-Black_54db1d61-f86f-4683-bc7b-ea83a6df4db4.jpg?v=1725828034&width=140"
+              loading="eager" class="client-logo-2" /><img alt=""
+              src="https://sachellecollective.myshopify.com/cdn/shop/files/SachelleCollective_Logo_Wordmark-Black_54db1d61-f86f-4683-bc7b-ea83a6df4db4.jpg?v=1725828034&width=140"
+              loading="eager" class="client-logo-2" /><img alt=""
+              src="https://sachellecollective.myshopify.com/cdn/shop/files/SachelleCollective_Logo_Wordmark-Black_54db1d61-f86f-4683-bc7b-ea83a6df4db4.jpg?v=1725828034&width=140"
+              loading="eager" class="client-logo-2" /><img alt=""
+              src="https://sachellecollective.myshopify.com/cdn/shop/files/SachelleCollective_Logo_Wordmark-Black_54db1d61-f86f-4683-bc7b-ea83a6df4db4.jpg?v=1725828034&width=140"
+              loading="eager" class="client-logo-2" /><img alt=""
+              src="https://sachellecollective.myshopify.com/cdn/shop/files/SachelleCollective_Logo_Wordmark-Black_54db1d61-f86f-4683-bc7b-ea83a6df4db4.jpg?v=1725828034&width=140"
+              loading="eager" class="client-logo-2" /><img alt=""
+              src="https://sachellecollective.myshopify.com/cdn/shop/files/SachelleCollective_Logo_Wordmark-Black_54db1d61-f86f-4683-bc7b-ea83a6df4db4.jpg?v=1725828034&width=140"
+              loading="eager" class="client-logo-2" />
+            <img alt=""
+              src="https://sachellecollective.myshopify.com/cdn/shop/files/SachelleCollective_Logo_Wordmark-Black_54db1d61-f86f-4683-bc7b-ea83a6df4db4.jpg?v=1725828034&width=140"
+              loading="eager" class="client-logo-2" /><img alt=""
+              src="https://sachellecollective.myshopify.com/cdn/shop/files/SachelleCollective_Logo_Wordmark-Black_54db1d61-f86f-4683-bc7b-ea83a6df4db4.jpg?v=1725828034&width=140"
+              loading="eager" class="client-logo-2" /><img alt=""
+              src="https://sachellecollective.myshopify.com/cdn/shop/files/SachelleCollective_Logo_Wordmark-Black_54db1d61-f86f-4683-bc7b-ea83a6df4db4.jpg?v=1725828034&width=140"
+              loading="eager" class="client-logo-2" /><img alt=""
+              src="https://sachellecollective.myshopify.com/cdn/shop/files/SachelleCollective_Logo_Wordmark-Black_54db1d61-f86f-4683-bc7b-ea83a6df4db4.jpg?v=1725828034&width=140"
+              loading="eager" class="client-logo-2" /><img alt=""
+              src="https://sachellecollective.myshopify.com/cdn/shop/files/SachelleCollective_Logo_Wordmark-Black_54db1d61-f86f-4683-bc7b-ea83a6df4db4.jpg?v=1725828034&width=140"
+              loading="eager" class="client-logo-2" /><img alt=""
+              src="https://sachellecollective.myshopify.com/cdn/shop/files/SachelleCollective_Logo_Wordmark-Black_54db1d61-f86f-4683-bc7b-ea83a6df4db4.jpg?v=1725828034&width=140"
+              loading="eager" class="client-logo-2" />
+          </div>
+          <div class="home-logo-left-gradient"></div>
+          <div class="home-logo-left-gradient right"></div>
+        </div>
+      </div>
+    </section>
+
+    <!-- services -->
+    <section class="services-section">
+      <div class="section-heading">
+        <h2 class="heading">Services We Provide</h2>
+        <div class="heading-svg">
+          <svg width="" height="" class="svg-path" viewBox="0 0 297 35" fill="" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M1.22961 19.3056C3.69595 15.4548 12.9448 12.4865 17.2609 11.4837C36.9917 6.67039 68.7458 4.26347 82.0024 5.46693C95.2591 6.67039 80.7693 9.07692 68.1292 11.4836C55.4892 13.8904 64.738 15.0937 76.1449 13.8904C87.5517 12.687 111.29 11.4836 131.638 10.882C151.985 10.2803 131.329 9.07692 111.29 9.07692C91.2513 9.07692 91.5595 6.67029 98.6503 5.46693C105.741 4.26357 121.772 -0.549874 140.578 0.0518062C159.384 0.653487 203.778 1.85685 222.893 6.06861C242.007 10.2804 281.468 21.7123 291.025 25.3224C300.583 28.9325 299.349 39.161 282.393 33.1442C265.437 27.1274 230.292 16.2972 153.218 14.4921C76.1449 12.6871 17.5691 22.9157 11.4032 27.1274C5.23738 31.3392 0.921268 31.3392 3.38762 27.7291C5.85396 24.119 6.47059 23.5174 4.00425 24.7207C1.5379 25.9241 -1.85333 24.119 1.22961 19.3056Z"
+              fill="#8AD2DC"></path>
+          </svg>
+        </div>
+      </div>
+      <div class="services">
+        <div class="service">
+          <a href="services/shopify-development.php">
+            <svg class="service-svg" xmlns="http://www.w3.org/2000/svg" width="" height="" fill="none"
+              viewBox="0 0 27 23">
+              <path fill=""
+                d="M24.823 10.173v10.294c0 .652-.227 1.205-.679 1.657a2.254 2.254 0 01-1.656.678H4.553a2.253 2.253 0 01-1.656-.678 2.254 2.254 0 01-.678-1.657V10.148c-.52-.427-.91-.981-1.172-1.663-.26-.68-.266-1.417-.016-2.206L2.338 2.01c.172-.543.464-.981.877-1.314.412-.333.905-.5 1.478-.5h17.63c.574 0 1.063.16 1.469.478.406.318.701.755.886 1.311L26.01 6.28c.25.79.245 1.523-.016 2.199a4.223 4.223 0 01-1.171 1.695zm-8.46-.934c.705 0 1.235-.215 1.59-.647.356-.431.501-.894.436-1.39l-.785-5.067H14.49v5.102a2 2 0 00.552 1.409c.367.396.808.593 1.321.593zm-5.813 0c.594 0 1.076-.197 1.447-.593.37-.396.555-.866.555-1.409V2.135H9.437l-.785 5.117c-.07.459.074.905.432 1.338.358.433.847.65 1.466.65zm-5.748 0c.479 0 .89-.166 1.235-.5.344-.334.557-.753.638-1.258l.76-5.346H4.693a.546.546 0 00-.336.093.557.557 0 00-.186.28L2.929 6.71c-.17.554-.09 1.115.241 1.68.331.566.875.848 1.632.848zm17.438 0c.698 0 1.233-.274 1.604-.823.371-.549.46-1.117.268-1.705l-1.306-4.228a.487.487 0 00-.186-.267.61.61 0 00-.336-.08h-2.677l.76 5.345c.08.505.293.924.638 1.258.344.334.756.5 1.235.5zM4.552 20.864h17.935a.386.386 0 00.285-.111.387.387 0 00.112-.286v-9.404c-.14.051-.258.083-.353.095a2.228 2.228 0 01-.293.019c-.58 0-1.092-.105-1.533-.316-.442-.21-.87-.547-1.284-1.01a4.137 4.137 0 01-1.286.966c-.495.24-1.06.36-1.694.36a3.604 3.604 0 01-1.55-.342 4.865 4.865 0 01-1.371-.985 4.2 4.2 0 01-1.357.985 3.704 3.704 0 01-1.535.342c-.583 0-1.13-.106-1.64-.317a3.492 3.492 0 01-1.345-1.01c-.544.543-1.044.9-1.502 1.071-.458.17-.904.256-1.34.256-.101 0-.206-.006-.313-.019a1.121 1.121 0 01-.333-.095v9.404c0 .116.037.211.112.286.074.074.17.111.285.111z">
+              </path>
+            </svg>
+            <h3 class="sub-heading">Shopify Development</h3>
+            <ul class="service-description">
+              <li>Shopify Store Setup & Customization</li>
+              <li>Shopify Theme Development</li>
+              <li>Shopify Store Redesign</li>
+              <li>Shopify Management & Store Maintenance</li>
+            </ul>
+            <div class="service-link">
+              <a href="#">
+                <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M20.3408 27.4683C20.5911 27.7186 20.9094 27.8359 21.2266 27.8359C21.5437 27.8359 21.862 27.7186 22.1123 27.4683L31.0415 18.5391C31.5267 18.0539 31.5267 17.2528 31.0415 16.7676L22.1123 7.8384C21.6271 7.35322 20.826 7.35322 20.3408 7.8384C19.8557 8.32358 19.8557 9.12466 20.3408 9.60984L28.3844 17.6534L20.3408 25.6969C19.8557 26.1821 19.8557 26.9831 20.3408 27.4683Z"
+                    fill="" stroke="" stroke-width="0.2"></path>
+                  <path
+                    d="M5.14821 18.9049H29.9059C30.5919 18.9049 31.1592 18.3376 31.1592 17.6516C31.1592 16.9657 30.5919 16.3983 29.9059 16.3983H5.14821C4.46223 16.3983 3.89492 16.9657 3.89492 17.6516C3.89492 18.3376 4.46223 18.9049 5.14821 18.9049Z"
+                    fill="" stroke="" stroke-width="0.2"></path>
+                </svg>
+              </a>
+            </div>
+          </a>
+        </div>
+        <div class="service">
+          <a href="services/website-design-and-development.html">
+            <svg class="service-svg" xmlns="http://www.w3.org/2000/svg" width="84" height="84" fill="none"
+              viewBox="0 0 84 84">
+              <mask id="mask0_1020_1442" width="" height="" x="0" y="0" maskUnits="userSpaceOnUse"
+                style="mask-type: alpha;">
+                <path fill="" d="M0 0H84V84H0z"></path>
+              </mask>
+              <g fill="" mask="url(#mask0_1020_1442)">
+                <path
+                  d="M41.42 72.981H15.08c-4.876 0-8.08-3.207-8.08-8.072V20.126c0-4.881 3.207-8.123 8.063-8.126h52.93c4.77 0 8.01 3.256 8.012 8.029V65.08c0 4.612-3.26 7.9-7.848 7.906-8.912.009-17.825.007-26.738-.006zm.14-42.512H11.33c-1.752 0-1.758 0-1.758 1.805V64.42c0 4.08 1.93 5.995 6.018 5.998h51.86c4.075 0 5.989-1.927 5.991-6.024V32.247c0-1.773 0-1.778-1.787-1.778H41.559zm-.05-2.586c10.12 0 20.24-.021 30.359.024 1.255 0 1.64-.398 1.596-1.614-.083-2.193 0-4.394-.043-6.59-.07-2.96-2.169-5.112-5.106-5.112-17.867-.023-35.733-.023-53.6 0-2.956 0-5.042 2.153-5.106 5.112-.051 2.153.059 4.305-.049 6.458-.067 1.361.38 1.754 1.745 1.749 10.055-.06 20.128-.035 30.205-.035v.008z">
+                </path>
+                <path
+                  d="M44.745 38.352c-.502 2.374-1.013 4.78-1.52 7.184-.981 4.634-1.96 9.269-2.939 13.906-.185.882-.443 1.805-1.556 1.614-1.177-.207-1.153-1.178-.954-2.128 1.414-6.737 2.83-13.474 4.247-20.21.062-.352.164-.696.303-1.025.27-.578.745-.923 1.403-.775.713.16.981.703 1.016 1.434zM34.266 56.385a1.943 1.943 0 01-1.228-.565c-2.182-1.851-4.378-3.689-6.534-5.57-1.142-.995-1.153-1.651-.021-2.644 2.185-1.916 4.413-3.786 6.63-5.667.63-.538 1.344-.869 2.003-.108.658.762.285 1.5-.393 2.083-1.626 1.402-3.225 2.83-4.902 4.179-.745.6-.83.97-.038 1.614 1.707 1.372 3.344 2.833 5.013 4.254.519.441.825.963.578 1.65a1.176 1.176 0 01-1.108.774zM48.771 56.385a1.168 1.168 0 01-1.12-.75c-.27-.684.021-1.214.537-1.658 1.631-1.394 3.225-2.828 4.91-4.168.842-.672.928-1.076 0-1.792-1.612-1.259-3.142-2.655-4.703-3.993-.702-.597-1.26-1.3-.538-2.166.723-.866 1.53-.341 2.204.232 2.18 1.854 4.373 3.694 6.518 5.588 1.03.907 1.038 1.572.027 2.457-2.22 1.948-4.486 3.84-6.736 5.75-.3.284-.687.46-1.099.5zM11.754 21.28a3.771 3.771 0 012.32-3.492c.457-.19.947-.288 1.443-.288a3.815 3.815 0 013.712 3.702 3.742 3.742 0 01-2.246 3.56 3.73 3.73 0 01-4.946-2.026 3.742 3.742 0 01-.272-1.456h-.011zM22.842 21.315a3.598 3.598 0 012.172-3.438c.443-.19.919-.289 1.4-.291a3.717 3.717 0 013.533 2.23c.198.454.304.942.31 1.437.041 1.967-1.776 3.789-3.762 3.783a3.679 3.679 0 01-3.653-3.72zM37.499 17.634a3.677 3.677 0 013.506 2.246c.193.455.292.944.291 1.438a3.855 3.855 0 01-3.674 3.732 3.67 3.67 0 01-3.468-2.304 3.681 3.681 0 01-.265-1.442 3.55 3.55 0 012.204-3.409 3.54 3.54 0 011.406-.26z">
+                </path>
+              </g>
+            </svg>
+            <h3 class="sub-heading">Website Design & Development</h3>
+            <ul class="service-description">
+              <li>Landing Page Design</li>
+              <li>Web Portals</li>
+              <li>Custom Website Design & Maintenance</li>
+            </ul>
+            <div class="service-link">
+              <a href="#">
+                <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M20.3408 27.4683C20.5911 27.7186 20.9094 27.8359 21.2266 27.8359C21.5437 27.8359 21.862 27.7186 22.1123 27.4683L31.0415 18.5391C31.5267 18.0539 31.5267 17.2528 31.0415 16.7676L22.1123 7.8384C21.6271 7.35322 20.826 7.35322 20.3408 7.8384C19.8557 8.32358 19.8557 9.12466 20.3408 9.60984L28.3844 17.6534L20.3408 25.6969C19.8557 26.1821 19.8557 26.9831 20.3408 27.4683Z"
+                    fill="" stroke="" stroke-width="0.2"></path>
+                  <path
+                    d="M5.14821 18.9049H29.9059C30.5919 18.9049 31.1592 18.3376 31.1592 17.6516C31.1592 16.9657 30.5919 16.3983 29.9059 16.3983H5.14821C4.46223 16.3983 3.89492 16.9657 3.89492 17.6516C3.89492 18.3376 4.46223 18.9049 5.14821 18.9049Z"
+                    fill="" stroke="" stroke-width="0.2"></path>
+                </svg>
+              </a>
+            </div>
+          </a>
+        </div>
+        <div class="service">
+          <a href="services/digital-marketing.html">
+            <svg class="service-svg" xmlns="http://www.w3.org/2000/svg" width="84" height="84" fill="none"
+              viewBox="0 0 84 84">
+              <mask id="mask0_1020_1442" width="" height="" x="0" y="0" maskUnits="userSpaceOnUse"
+                style="mask-type: alpha;">
+                <path fill="" d="M0 0H84V84H0z"></path>
+              </mask>
+              <g fill="" mask="url(#mask0_1020_1442)">
+                <path
+                  d="M41.42 72.981H15.08c-4.876 0-8.08-3.207-8.08-8.072V20.126c0-4.881 3.207-8.123 8.063-8.126h52.93c4.77 0 8.01 3.256 8.012 8.029V65.08c0 4.612-3.26 7.9-7.848 7.906-8.912.009-17.825.007-26.738-.006zm.14-42.512H11.33c-1.752 0-1.758 0-1.758 1.805V64.42c0 4.08 1.93 5.995 6.018 5.998h51.86c4.075 0 5.989-1.927 5.991-6.024V32.247c0-1.773 0-1.778-1.787-1.778H41.559zm-.05-2.586c10.12 0 20.24-.021 30.359.024 1.255 0 1.64-.398 1.596-1.614-.083-2.193 0-4.394-.043-6.59-.07-2.96-2.169-5.112-5.106-5.112-17.867-.023-35.733-.023-53.6 0-2.956 0-5.042 2.153-5.106 5.112-.051 2.153.059 4.305-.049 6.458-.067 1.361.38 1.754 1.745 1.749 10.055-.06 20.128-.035 30.205-.035v.008z">
+                </path>
+                <path
+                  d="M44.745 38.352c-.502 2.374-1.013 4.78-1.52 7.184-.981 4.634-1.96 9.269-2.939 13.906-.185.882-.443 1.805-1.556 1.614-1.177-.207-1.153-1.178-.954-2.128 1.414-6.737 2.83-13.474 4.247-20.21.062-.352.164-.696.303-1.025.27-.578.745-.923 1.403-.775.713.16.981.703 1.016 1.434zM34.266 56.385a1.943 1.943 0 01-1.228-.565c-2.182-1.851-4.378-3.689-6.534-5.57-1.142-.995-1.153-1.651-.021-2.644 2.185-1.916 4.413-3.786 6.63-5.667.63-.538 1.344-.869 2.003-.108.658.762.285 1.5-.393 2.083-1.626 1.402-3.225 2.83-4.902 4.179-.745.6-.83.97-.038 1.614 1.707 1.372 3.344 2.833 5.013 4.254.519.441.825.963.578 1.65a1.176 1.176 0 01-1.108.774zM48.771 56.385a1.168 1.168 0 01-1.12-.75c-.27-.684.021-1.214.537-1.658 1.631-1.394 3.225-2.828 4.91-4.168.842-.672.928-1.076 0-1.792-1.612-1.259-3.142-2.655-4.703-3.993-.702-.597-1.26-1.3-.538-2.166.723-.866 1.53-.341 2.204.232 2.18 1.854 4.373 3.694 6.518 5.588 1.03.907 1.038 1.572.027 2.457-2.22 1.948-4.486 3.84-6.736 5.75-.3.284-.687.46-1.099.5zM11.754 21.28a3.771 3.771 0 012.32-3.492c.457-.19.947-.288 1.443-.288a3.815 3.815 0 013.712 3.702 3.742 3.742 0 01-2.246 3.56 3.73 3.73 0 01-4.946-2.026 3.742 3.742 0 01-.272-1.456h-.011zM22.842 21.315a3.598 3.598 0 012.172-3.438c.443-.19.919-.289 1.4-.291a3.717 3.717 0 013.533 2.23c.198.454.304.942.31 1.437.041 1.967-1.776 3.789-3.762 3.783a3.679 3.679 0 01-3.653-3.72zM37.499 17.634a3.677 3.677 0 013.506 2.246c.193.455.292.944.291 1.438a3.855 3.855 0 01-3.674 3.732 3.67 3.67 0 01-3.468-2.304 3.681 3.681 0 01-.265-1.442 3.55 3.55 0 012.204-3.409 3.54 3.54 0 011.406-.26z">
+                </path>
+              </g>
+            </svg>
+            <h3 class="sub-heading">Digital Marketing</h3>
+            <ul class="service-description">
+              <li>Paid Advertising (PPC and Facebook Ads)</li>
+              <li>SEO</li>
+              <li>Email Marketing</li>
+              <li>Social Media Management</li>
+            </ul>
+            <div class="service-link">
+              <a href="#">
+                <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M20.3408 27.4683C20.5911 27.7186 20.9094 27.8359 21.2266 27.8359C21.5437 27.8359 21.862 27.7186 22.1123 27.4683L31.0415 18.5391C31.5267 18.0539 31.5267 17.2528 31.0415 16.7676L22.1123 7.8384C21.6271 7.35322 20.826 7.35322 20.3408 7.8384C19.8557 8.32358 19.8557 9.12466 20.3408 9.60984L28.3844 17.6534L20.3408 25.6969C19.8557 26.1821 19.8557 26.9831 20.3408 27.4683Z"
+                    fill="" stroke="" stroke-width="0.2"></path>
+                  <path
+                    d="M5.14821 18.9049H29.9059C30.5919 18.9049 31.1592 18.3376 31.1592 17.6516C31.1592 16.9657 30.5919 16.3983 29.9059 16.3983H5.14821C4.46223 16.3983 3.89492 16.9657 3.89492 17.6516C3.89492 18.3376 4.46223 18.9049 5.14821 18.9049Z"
+                    fill="" stroke="" stroke-width="0.2"></path>
+                </svg>
+              </a>
+            </div>
+          </a>
+        </div>
+      </div>
+    </section>
+
+    <!-- call to action -->
+    <?php
+    include('./sections/call-to-action.php');
+    ?>
+
+ <!-- contact us section -->
+  <?php
+  include('./sections/contact-us.php');
+  ?>
+
+  </div>
+
+  <!-- footer -->
+ <?php
+ include('./sections/footer.php')
+ ?>
+<!-- scripts -->
+ <?php
+ include('./sections/scripts.php');
+ ?>
+
+</body>
+
+</html>
---
+++ v2/sections/scripts.php
@@ -0,0 +1,1 @@
+ <script src="./assets/script/App.js" defer></script>
