a3b512e30ab13f8be467d0f5c665594f8533cfe6
Muhammad Owais <owais.quest@gmail.com>
2024-10-01 10:27:11 +0500

    new sections created

---
+++ v2/contact-us.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Contact Us | Qonkar Technologies</title>
+
+  <?php
+  include('./sections/head.php');
+  ?>
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="./assets/css/style.css" />
+
+</head>
+
+<body>
+  <?php
+include('./sections/progress-bar.php')
+?>
+  <?php
+  include('./sections/nav-bar.php');
+  ?>
+
+  <div class="main container">
+
+    <?php 
+    include('./sections/contact-us.php');
+    ?>
+
+  </div>
+
+  <!-- footer -->
+  <?php
+ include('./sections/footer.php')
+ ?>
+
+  <!-- scripts -->
+  <?php
+ include('./sections/scripts.php');
+ ?>
+
+  <script src="./assets/script/App.js" defer></script>
+</body>
+
+</html>
--- v2/sections/contact-us.php
+++ v2/sections/contact-us.php
@@ -1,68 +1,76 @@
-<div class="services_location_banner">
-      <div class="row">
-        <div class="my-auto col-xxl-5 col-xl-5 col-lg-5 col-md-6 col-12">
-          <div class="banenr_content">
-            <div class="example" data-text="Get A Quote">
-              <h1 class="Heading-service">Let's start a project together</h1>
-            </div>
-            <p>
-              We'll contact you within a couple of hours to schedule a meeting
-              to discuss your goals.
-            </p>
+<section class="section-contact-us">
+  <div class="form-sections">
+    <div class="form-container">
+      <div class="details-section">
+        <div class="section-heading">
+          <h2 class="heading">Let's Connect With Our Experts</h2>
+          <div class="heading-svg">
+            <svg class="svg-path" viewBox="0 0 297 35" fill="" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M1.22961 19.3056C3.69595 15.4548 12.9448 12.4865 17.2609 11.4837C36.9917 6.67039 68.7458 4.26347 82.0024 5.46693C95.2591 6.67039 80.7693 9.07692 68.1292 11.4836C55.4892 13.8904 64.738 15.0937 76.1449 13.8904C87.5517 12.687 111.29 11.4836 131.638 10.882C151.985 10.2803 131.329 9.07692 111.29 9.07692C91.2513 9.07692 91.5595 6.67029 98.6503 5.46693C105.741 4.26357 121.772 -0.549874 140.578 0.0518062C159.384 0.653487 203.778 1.85685 222.893 6.06861C242.007 10.2804 281.468 21.7123 291.025 25.3224C300.583 28.9325 299.349 39.161 282.393 33.1442C265.437 27.1274 230.292 16.2972 153.218 14.4921C76.1449 12.6871 17.5691 22.9157 11.4032 27.1274C5.23738 31.3392 0.921268 31.3392 3.38762 27.7291C5.85396 24.119 6.47059 23.5174 4.00425 24.7207C1.5379 25.9241 -1.85333 24.119 1.22961 19.3056Z"
+                fill="#8AD2DC"></path>
+            </svg>
           </div>
         </div>
-        <div
-          class="my-auto offset-lg-2 col-xxl-5 col-xl-5 col-lg-5 col-md-6 col-12"
-        >
-          <div class="banner_form">
-            <h2>Book a Free <span>Consultation</span></h2>
-            <form method="POST" class="banner_form_inner">
-              <div class="from_item">
-                <input
-                  placeholder="Full Name"
-                  name="name"
-                  type="text"
-                  class="form-control"
-                />
-              </div>
-              <div class="from_item">
-                <input
-                  placeholder="Email"
-                  name="email"
-                  type="email"
-                  class="form-control"
-                />
-              </div>
-              <div class="from_item">
-                <input
-                  placeholder="Number"
-                  name="phone"
-                  type="tel"
-                  class="form-control"
-                />
-              </div>
-              <div class="from_item">
-                <textarea
-                  rows="4"
-                  name="description"
-                  placeholder="Describe Your Project Need."
-                  class="form-control"
-                ></textarea>
-              </div>
-              <p class="submitting_policy">
-                By submitting this form, you agree to our<a
-                  href="/privacy-policy"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Privacy Policy</a
-                >
-              </p>
-              <button type="submit" class="btn_submit btn btn-primary">
-                Get In Touch
-              </button>
-            </form>
+        <p>
+          Get valuable consultation from our professionals to discuss your mobile application development project. We
+          are here to help you with all of your queries.
+        </p>
+        <h2 class="sub-heading">Revolutionize Your Business</h2>
+        <p>
+          Collaborate with us and become a trendsetter with our reliable mobile application development company.
+        </p>
+        <div class="badges">
+          <div class="badge">
+            <img src="" alt="">
+            <div class="rating">
+              <p>5.0</p>
+              <p>Goodfirms</p>
+            </div>
           </div>
+          <div class="badge">
+            <img src="" alt="">
+            <div class="rating">
+              <p>5.0</p>
+              <p>Goodfirms</p>
+            </div>
+          </div>
+          <div class="badge">
+            <img src="" alt="">
+            <div class="rating">
+              <p>5.0</p>
+              <p>Goodfirms</p>
+            </div>
+          </div>
+        </div>
+        <p>Contact Info:</p>
+        <div class="contact-info">
+          <img src="" alt="">
+          <p>info@qonkar.com</p>
+        </div>
+        <div class="contact-info">
+          <img src="" alt="">
+          <p></p>
         </div>
       </div>
+      <div class="form-section">
+        <h2>Contact Us</h2>
+        <form>
+          <label for="fullName">Full Name</label>
+          <input type="text" id="fullName" name="fullName" required>
+
+          <label for="email">Email</label>
+          <input type="email" id="email" name="email" required>
+
+          <label for="number">Number</label>
+          <input type="tel" id="number" name="number" required>
+
+          <label for="aboutProject">About Project</label>
+          <textarea id="aboutProject" name="aboutProject" rows="4" required></textarea>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
+  </div>
+</section>
