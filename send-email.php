c1954f340a36a0fe337c63803a9519c2fca8be56
Muhammad Owais <owais.quest@gmail.com>
2024-10-04 18:59:03 +0500

    PHPMailer Added

--- send-email.php
+++ send-email.php
@@ -1,24 +1,34 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    $fullName = htmlspecialchars($_POST['fullName']);
-    $email = htmlspecialchars($_POST['email']);
-    $number = htmlspecialchars($_POST['number']);
-    $aboutProject = htmlspecialchars($_POST['aboutProject']);
 
-    
-    $to = 'hr@qonkar.com'; 
-    $subject = 'New Contact Form Submission';
-    $message = "Full Name: $fullName\nEmail: $email\nPhone Number: $number\nProject Details: $aboutProject";
-    $headers = "From: $email";
+require '/PHPMailer/src/Exception.php';
+require '/PHPMailer/src/PHPMailer.php';
+require '/PHPMailer/src/SMTP.php';
+$mail = new PHPMailer(true);
 
-    
-    if (mail($to, $subject, $message, $headers)) {
-        echo 'Email has been sent successfully!';
-    } else {
-        echo 'There was an error sending the email.';
-    }
-} else {
-    echo 'Invalid request method.';
+try {
+    // Server settings
+    $mail->isSMTP();                                    // Send using SMTP
+    $mail->Host       = 'mail.qonkar.com';          // Set the SMTP server to send through (replace with your actual SMTP host)
+    $mail->SMTPAuth   = true;                           // Enable SMTP authentication
+    $mail->Username   = 'hr@qonkar.com';                // SMTP username
+    $mail->Password   = 'Hp.dell.39.72';          // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption; use `PHPMailer::ENCRYPTION_SMTPS` for SSL
+    $mail->Port       = 587;                            // TCP port to connect to
+
+    // Recipients
+    $mail->setFrom('hr@qonkar.com', 'Qonkar HR');       // Sender address and name
+    $mail->addAddress('mowaisrehmani@gmail.com');         // Add a recipient (replace with actual recipient)
+
+    // Content
+    $mail->isHTML(true);                                // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the plain text message body for non-HTML email clients';
+
+    // Send the email
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
 ?>
