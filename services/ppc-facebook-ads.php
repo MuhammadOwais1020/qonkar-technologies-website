49c8bef525a1500af543ad9cc32eaf1ef69fc631
Hakim zadi <kinzanawaz585@gmail.com>
2024-12-04 05:22:04 -0800

    Update ppc-facebook-ads.php

--- services/ppc-facebook-ads.php
+++ services/ppc-facebook-ads.php
@@ -58,11 +58,8 @@ include(__DIR__ . '/../sections/progress-bar.php')
       <div class="hero-section-image">
         <div class="wrapper-image">
         <img src="https://qonkar.com/assets/image/Qonkar-technology-hero-sec.webp" alt="Qonkar Technologies Hero Section image">
-
-
         </div>
         <div class="wrapper-inner-image">
-          
         </div>
       </div>
     </section>
