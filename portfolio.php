4721a6e80d9fbf34922716d7d5d8adf4db8aed10
Hakim zadi <kinzanawaz585@gmail.com>
2025-01-14 02:11:10 -0800

    Update portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -6,8 +6,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="responsive.css">
+    <link rel="stylesheet" href="https://qonkar.com/assets/css/style.css" />
+  <link rel="stylesheet" href="https://qonkar.com/assets/css/responsive.css">
+
     <title>Portfolio</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -15,9 +16,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
-  <?php
-  include('./sections/head.php');
-  ?>
+  
 </head>
 <body>
     <section class="portfolio_page">
