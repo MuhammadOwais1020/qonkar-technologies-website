d860c6ceb7f6294ab92b264475246776e0ee9ca7
Hakim zadi <kinzanawaz585@gmail.com>
2025-01-14 02:29:26 -0800

    Update portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio</title>
-
+    
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
@@ -15,20 +15,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
   <?php
-  include(__DIR__ . '/../sections/head.php');
-  ?>
-</head>
-
-<body>
-  <?php 
-  include('./sections/gtm-body.php')
-  ?>
-  <?php
-include(__DIR__ . '/../sections/progress-bar.php')
-?>
-
-  <?php
-  include(__DIR__ . '/../sections/nav-bar.php');
+  include('./sections/head.php');
   ?>
 </head>
 <body>
