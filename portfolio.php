8abfdddc510029164b07000bf71a165e5737051d
Hakim zadi <kinzanawaz585@gmail.com>
2025-01-14 02:11:43 -0800

    Update portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -16,7 +16,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
-  
+  <?php
+  include('./sections/head.php');
+  ?>
 </head>
 <body>
     <section class="portfolio_page">
