f10b2f4fc6da478eed69b1627f6f1b84cbf8524b
Hakim zadi <kinzanawaz585@gmail.com>
2025-01-14 02:12:30 -0800

    Update portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -6,8 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://qonkar.com/assets/css/style.css" />
-  <link rel="stylesheet" href="https://qonkar.com/assets/css/responsive.css">
+    
 
     <title>Portfolio</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
