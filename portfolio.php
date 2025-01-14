61de8fcbb5f690b40a90c5b84ff9b21da5ba53fc
Hakim zadi <kinzanawaz585@gmail.com>
2025-01-14 02:06:43 -0800

    Update portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -6,8 +6,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="responsive.css">
     <title>Portfolio</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -15,6 +13,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <?php
+  include('./sections/head.php');
+  ?>
 </head>
 <body>
     <section class="portfolio_page">
