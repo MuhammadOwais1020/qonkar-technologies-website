959d6d84249df75441c992a274b8d940513773e8
Hakim zadi <kinzanawaz585@gmail.com>
2025-01-16 02:30:30 -0800

    Update portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -64,7 +64,7 @@
     </section>
     <div class="pdf-section">
         <div class="pdf-heading">Learn More About Pesticides</div>
-        <a href="https://qonkar.com/qonkar-technologies-website/Pesticide/Proposal Updated.pdf" class="pdf-button" target="_blank">View Pesticides PDF</a>
+        <a href="https://qonkar.com/qonkar-technologies-website/Pesticide-Proposal-Updated.pdf" class="pdf-button" target="_blank">View Pesticides PDF</a>
     </div>
 </body>
 </html>
